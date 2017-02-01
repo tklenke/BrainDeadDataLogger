ca334e14322652375a2b9f163b38fa47be8ea8cc
Tom Klenke <tklenke@users.noreply.github.com>
2017-01-31 16:28:38 -0800

    added routes for initializing and logging data stream

--- fwww/bdl.py
+++ fwww/bdl.py
@@ -1,15 +1,96 @@
 # app.py or app/__init__.py
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import re
+import threading
+import time
+import os
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 # Now we can access the configuration variables via app.config["VAR_NAME"].
-app.config["DEBUG"]
+DEBUG=app.config["DEBUG"]
 
 @app.route("/")
 def home():
     return render_template('home.html')
+    
+@app.route("/initstream/<stream_name>")
+def initialize_stream(stream_name):
+	#validate stream name (alpha numeric only)
+	r = re.compile('[\W]+')
+	if r.search(stream_name):
+		return ("0 invalid stream name:{}".format(stream_name))
+		
+	#create header row for CSV file
+	cols = sorted(request.args.keys(), key=int)  #need to convert to integer during sort
+	hrow = '"timestamp"'
+	numcols = 0
+	for c in cols:
+		hrow += ',"' + request.args[c].replace('"','""') + '"'
+		numcols +=1
+		
+	#check to see if max columns exceeded
+	if numcols > app.config["MAX_COLUMNS"]:
+		return ("0 number of columns {} exceeds max columns of {}".format(numcols,app.config["MAX_COLUMNS"]))
+		
+	#check to see if the file exists -- if so then move it, otherwise open it
+	fname = "{}/{}.csv".format(app.config["PATH_TO_DATA"],stream_name) 
+
+	if os.path.isfile(fname):	
+		#create filename (path + stream name + year,mon,day,h,m,s)
+		foldname = "{}/{}_{}.csv".format(app.config["PATH_TO_DATA"],stream_name,time.strftime("%Y_%m_%d_%H_%M_%S"))
+		os.rename(fname,foldname)
+		
+	#open file and write header row
+	lock = threading.Lock()
+	with lock:
+		f = open(fname,'w')
+		f.write(hrow + "\r\n")	
+		f.close()
+		
+	return ("1 {} stream initialized with {} columns".format(stream_name,numcols))	
+#	return render_template('test.html',stream_name=stream_name, hrow=hrow, fname=fname)
+
+@app.route("/log/<stream_name>")
+def log_stream(stream_name):
+	#check to see if the file exists
+	fname = "{}/{}.csv".format(app.config["PATH_TO_DATA"],stream_name) 
+	if os.path.isfile(fname) == False:
+		return ("0 {} stream not initialized".format(stream_name))
+	
+	#create row for CSV file
+	cols = sorted(request.args.keys(), key=int)  #need to convert to integer during sort
+	maxcol = 0
+	if len(cols) > 1:
+		maxcol = int(cols[-1])
+	else:
+		maxcol = len(cols)
+		
+	numdata = 0
+	numcols = 0
+	row = '"{}",'.format(time.strftime(app.config['TIMESTAMP_FORMAT']))
+	for c in range(1,maxcol+1):
+		if 	request.args.get(str(c),None) != None:  #count number of entries with data
+			numdata += 1
+			#put non-numeric data in quotes
+			r = re.compile('^-?(0|[1-9]\d*)(\.\d+)?$')
+			s = str(request.args[str(c)])
+			if r.match(s):
+				row += ',' + s
+			else:
+				row += ',"' + s.replace('"','""') + '"'
+			
+		numcols += 1
+		
+	#open file and write data row
+	lock = threading.Lock()
+	with lock:
+		f = open(fname,'a')
+		f.write(row + "\r\n")
+		f.close()		
+	
+	return ("1 {} data elements in {} columns written to stream {}".format(numdata, numcols, stream_name))	
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0')
--- fwww/config.py
+++ fwww/config.py
@@ -2,3 +2,12 @@
 
 #remember to set to False in production
 DEBUG = True
+
+#path to directory we want to store the data files in
+PATH_TO_DATA = '/home/tom/Projects/bdl/data'
+
+#maximum number of columns the app will permit 
+MAX_COLUMNS = 30
+
+#format for timestamps, change it if you like
+TIMESTAMP_FORMAT = "%d-%b-%Y %H:%M:%S"
