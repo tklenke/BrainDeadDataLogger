b5cc61ad9e161dbfd7b2346a29ad7969d1ec3b4c
Tom Klenke <tklenke@users.noreply.github.com>
2017-02-23 20:29:24 -0800

    added config_EXAMPLE.py

---
+++ fwww/config_EXAMPLE.py
@@ -0,0 +1,19 @@
+# configuration variables my app needs
+
+#remember to set to False in production
+DEBUG = True
+
+#key to enable initialize and logging
+UPDATE_KEY = "put_your_key_here"
+
+#path to directory we want to store the data files in
+PATH_TO_DATA = '<path to data directory>'
+
+#maximum number of columns the app will permit 
+MAX_COLUMNS = 30
+
+#format for timestamps, change it if you like
+TIMESTAMP_FORMAT = "%d-%b-%Y %H:%M:%S"
+
+#default number of chart rows
+DEFAULT_CHART_ROWS = 100
